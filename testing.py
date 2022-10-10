7a59a9682cb9931b8489184ddab22b2ca11dc305
Dishant Tekwani <73080020+dishantekwanii@users.noreply.github.com>
2022-10-10 22:15:10 +0100

    done

---
+++ testing.py
@@ -0,0 +1,11 @@
+from Calculator import multiply
+def test_mult():
+    assert multiply(10,11) -- 110
+
+from Calculator import add
+def test_add():
+    assert add(10,11) -- 21
+
+from Calculator import subtract
+def test_sub():
+    assert subtract(6,5) -- 1
